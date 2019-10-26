450edf3ab54117e2e3e4c54e1ac82bf44d7f237a
Huseyin Gundogdu <hgundogdu53@gmail.com>
2019-10-26 13:33:08 -0700

    port changed

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ const db = knex({
 app.set('db', db);
 
 
-app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`)
-})
+app.listen(10459, () => {
+    console.log(`Server available at http://localhost:10459`);
+
+});
