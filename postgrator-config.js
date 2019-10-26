c71641b94b974f395cfc712c26c3ff43646c9515
Huseyin Gundogdu <hgundogdu53@gmail.com>
2019-10-26 14:16:44 -0700

    versioned

--- postgrator-config.js
+++ postgrator-config.js
@@ -7,10 +7,5 @@ module.exports = {
         ? process.env.TEST_DATABASE_URL
         : process.env.DATABASE_URL,
     "ssl": !!process.env.SSL,
-    "host": process.env.MIGRATION_DATABASE_HOST,
-    "port": process.env.MIGRATION_DATABASE_PORT,
-    "database": process.env.MIGRATION_DATABASE_NAME,
-    "username": process.env.MIGRATION_DATABASE_USER,
-    "password": process.env.MIGRATION_DATABASE_PASS
 
 }
