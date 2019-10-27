a4e79b9202181a34722fb3557dc064d1271ffa11
Huseyin Gundogdu <hgundogdu53@gmail.com>
2019-10-27 11:51:11 -0700

    postgres fixed

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    PORT: process.env.PORT || 8001,
+    PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgres://welnarcfvqtyrp:c0710755983b1456d4b236b265e174cb74f68d0b72f398849e879edb1a667fb5@ec2-54-83-202-132.compute-1.amazonaws.com:5432/ddlv7hrb22999n',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/blogful-test'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://hsy-53@localhost/noteful-db'
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://hsy-53@localhost/noteful-db-test'
 
 }
