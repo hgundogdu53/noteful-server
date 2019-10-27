e630e77e1c1230c297957f2ddd6e030877cc04b5
Huseyin Gundogdu <hgundogdu53@gmail.com>
2019-10-26 22:54:22 -0700

    postgres v2

--- postgrator-config.js
+++ postgrator-config.js
@@ -7,5 +7,10 @@ module.exports = {
         ? process.env.TEST_DATABASE_URL
         : process.env.DATABASE_URL,
     "ssl": !!process.env.SSL,
+    "host": process.env.Host,
+    "port": process.env.PORT,
+    "database": process.env.Database,
+    "username": process.env.User,
+    "password": process.env.Password
 
 }
