550caf7720ca6d297dc0fb9352bd2d7f3e7efba0
Huseyin Gundogdu <hgundogdu53@gmail.com>
2019-11-04 23:23:47 -0800

    some changes happened

--- src/notes/notes-router.js
+++ src/notes/notes-router.js
@@ -8,11 +8,10 @@ const jsonParser = express.json()
 
 const serializeNote = note => ({
     id: note.id,
-    name: xss(note.name),
+    note_name: xss(note.name),
     folder_id: note.folder_id,
     content: xss(note.content),
     modified: note.modified,
-    date_published: note.date_published,
 })
 
 notesRouter
@@ -27,8 +26,8 @@ notesRouter
             .catch(next)
     })
     .post(jsonParser, (req, res, next) => {
-        const { name, folder_id, content } = req.body
-        const newNote = { name, folder_id, content }
+        const { note_name, folder_id, content } = req.body
+        const newNote = { note_name, folder_id, content }
         NotesService.insertNote(
             req.app.get('db'),
             newNote
@@ -37,7 +36,7 @@ notesRouter
                 res
                     .status(201)
                     .location(path.posix.join(req.originalUrl, `/${note.id}`))
-                    .json(note)
+                    .json(serializeNote(note));
             })
             .catch(next)
     })
@@ -74,14 +73,14 @@ notesRouter
             .catch(next)
     })
     .patch(jsonParser, (req, res, next) => {
-        const { name, folder_id, content } = req.body
-        const noteToUpdate = { name, folder_id, content }
+        const { note_name, folder_id, content } = req.body
+        const noteToUpdate = { note_name, folder_id, content }
 
         const numberOfValues = Object.values(noteToUpdate).filter(Boolean).length
         if (numberOfValues === 0) {
             return res.status(400).json({
                 error: {
-                    message: `Request body must contain either 'name', 'folder_id' or 'content'`
+                    message: `Request body must contain either 'note_name', 'folder_id' or 'content'`
                 }
             })
         }
